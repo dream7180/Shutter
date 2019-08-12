52f46eefa6ee723264e9a8fd9802d2b21d7bd51f
dream7180 <dreamawake@163.com>
2019-08-12 11:23:02 +0800

    Update DlgListCtrl.cpp

--- src/DlgListCtrl.cpp
+++ src/DlgListCtrl.cpp
@@ -168,8 +168,9 @@ void DlgListCtrl::SelectFont(CDC& dc)
 		HFONT hfont = static_cast<HFONT>(::GetStockObject(DEFAULT_GUI_FONT));
 		::GetObject(hfont, sizeof(lf), &lf);
 		lf.lfWeight = FW_NORMAL;
-		_tcscpy(lf.lfFaceName, _T("Segoe UI"));
-		lf.lfQuality = ANTIALIASED_QUALITY;
+		lf.lfHeight += 1;
+		_tcscpy(lf.lfFaceName, _T("Tahoma"));
+		//lf.lfQuality = ANTIALIASED_QUALITY;
 		_font.CreateFontIndirect(&lf);
 		dc.SelectObject(&_font);
 		//dc.SelectStockObject(DEFAULT_GUI_FONT);
