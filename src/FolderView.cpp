70deb543e92292c2f489dac54e6d2f2211fb7636
dream7180 <dreamawake@163.com>
2019-08-12 11:24:11 +0800

    Update FolderView.cpp

--- src/FolderView.cpp
+++ src/FolderView.cpp
@@ -81,8 +81,9 @@ bool FolderView::Create(CWnd* parent, UINT id, FolderPathPtr path/*= 0*/, bool r
 	LOGFONT lf;
 	HFONT hfont = static_cast<HFONT>(::GetStockObject(DEFAULT_GUI_FONT));
 	::GetObject(hfont, sizeof(lf), &lf);
-	lf.lfQuality = ANTIALIASED_QUALITY;
-	_tcscpy(lf.lfFaceName, _T("Segoe UI"));
+	//lf.lfQuality = ANTIALIASED_QUALITY;
+	lf.lfHeight += 1;
+	_tcscpy(lf.lfFaceName, _T("Tahoma"));
 	hfont = CreateFontIndirectW(&lf);
 	SendMessage(WM_SETFONT, WPARAM(hfont));
 
