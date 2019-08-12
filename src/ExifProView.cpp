7980b7ac7362ce263adf7b28ce7755bf9ea34ff1
dream7180 <dreamawake@163.com>
2019-08-12 11:23:09 +0800

    Update ExifProView.cpp

--- src/ExifProView.cpp
+++ src/ExifProView.cpp
@@ -535,8 +535,9 @@ void ExifView::InitialUpdate()
 	LOGFONT lf;
 	HFONT hfont = static_cast<HFONT>(::GetStockObject(DEFAULT_GUI_FONT));
 	::GetObject(hfont, sizeof(lf), &lf);
-	lf.lfQuality = ANTIALIASED_QUALITY;
-	_tcscpy(lf.lfFaceName, _T("Segoe UI"));
+	//lf.lfQuality = ANTIALIASED_QUALITY;
+	lf.lfHeight += 1;
+	_tcscpy(lf.lfFaceName, _T("Tahoma"));
 	hfont = CreateFontIndirectW(&lf);
 	SendMessage(WM_SETFONT, WPARAM(hfont));
 	//SendMessage(WM_SETFONT, WPARAM(::GetStockObject(DEFAULT_GUI_FONT)));
