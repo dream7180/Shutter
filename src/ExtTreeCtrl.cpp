f03e200779693423e5a4d512a2b05df04f6fdb34
dream7180 <dreamawake@163.com>
2019-08-12 11:23:13 +0800

    Update ExtTreeCtrl.cpp

--- src/ExtTreeCtrl.cpp
+++ src/ExtTreeCtrl.cpp
@@ -148,8 +148,9 @@ bool ExtTreeCtrl::CreateTree()
 	LOGFONT lf;
 	HFONT hfont = static_cast<HFONT>(::GetStockObject(DEFAULT_GUI_FONT));
 	::GetObject(hfont, sizeof(lf), &lf);
-	lf.lfQuality = ANTIALIASED_QUALITY;
-	_tcscpy(lf.lfFaceName, _T("Segoe UI"));
+	//lf.lfQuality = ANTIALIASED_QUALITY;
+	lf.lfHeight += 1;
+	_tcscpy(lf.lfFaceName, _T("Tahoma"));
 	hfont = CreateFontIndirectW(&lf);
 
 	header_wnd_.SendMessage(WM_SETFONT, WPARAM(hfont));
